b80817ec76595e9aa4a64867fba024fd6789e47b
Harshit kumar <122025308+hrshit@users.noreply.github.com>
2024-01-19 13:11:44 +0530

    Adding some components and styles

--- trace/app/page.tsx
+++ trace/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import ProductCard from './Components/ProductCard'
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
       <Link href="/Users"> Users </Link>
+      <ProductCard />
     </main>
   )
 }
